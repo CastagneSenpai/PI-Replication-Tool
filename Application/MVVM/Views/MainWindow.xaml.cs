03ac866c65ea934e7aab52aee34318e410f17e82
Romain Castagne <romain.castagne@external.total.com>
2022-01-11 15:42:12 +0100

    Main interface avec liste serveurs

--- Application/MVVM/Views/MainWindow.xaml.cs
+++ Application/MVVM/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using PI_Replication_Tool.MVVM.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,21 +23,28 @@ namespace PI_Replication_Tool
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public ObservableCollection 
+
         public MainWindow()
         {
             InitializeComponent();
 
-            List<string> PIServerNames = new List<string>();
-            foreach (var server in PIServers.GetPIServers())
-            {
-                PIServerNames.Add(server.Name);
-            }
+            //List<string> list = new List<string>();
+            //list.Add("ezkdos"); list.Add("561616"); list.Add("cksdc64");
+
+            //list<string> piservernames = new list<string>();
+            //foreach (var server in piservers.getpiservers())
+            //{
+            //    piservernames.add(server.name);
+            //}
+
+            var PILocalServers = PIServers.GetPIServers();
 
             // Chargement de la liste des serveurs sources
-            listSourceServer.ItemsSource = PIServerNames;
+            listSourceServer.ItemsSource = PILocalServers;
 
             // Chargement de la liste des serveurs cibles
-            listTargetServer.ItemsSource = PIServerNames;
+            listTargetServer.ItemsSource = PILocalServers;
         }
 
         private void Button_Continue_Click(object sender, RoutedEventArgs e)
