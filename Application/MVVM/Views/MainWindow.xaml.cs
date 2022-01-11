77e30a22d17bdd962cfc7d330eece82789c3e8a1
Romain Castagne <romain.castagne@external.total.com>
2022-01-11 18:03:13 +0100

    Revision de la grille + Modifs barre navigation

--- Application/MVVM/Views/MainWindow.xaml.cs
+++ Application/MVVM/Views/MainWindow.xaml.cs
@@ -70,6 +70,31 @@ namespace PI_Replication_Tool
             //}
             
         }
-    
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed) 
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowsStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else 
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
     }
 }
